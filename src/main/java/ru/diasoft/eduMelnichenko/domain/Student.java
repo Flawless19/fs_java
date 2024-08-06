6655b83ebae676c574aef6254dbe92f7800753d4
Alena Melnichenko <melnichenko2001@gmail.com>
2024-08-06 19:46:26 +0300

    Модуль 7.  Добавлен класс Student

---
+++ src/main/java/ru/diasoft/eduMelnichenko/domain/Student.java
@@ -0,0 +1,29 @@
+package ru.diasoft.eduMelnichenko.domain;
+
+public class Student extends Person {
+
+    String group;
+    String course;
+
+    public Student(String firstName, String secondName, int age, String group, String course) {
+        super(firstName, secondName, age);
+        this.group = group;
+        this.course = course;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+}
